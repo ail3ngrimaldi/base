c4de666b22244dc574c52b6485ca3872901dc6c4
ail3n <ailenrgrimaldi@gmail.com>
2025-02-05 23:43:45 -0300

    Add new elements and create utils for reusable code

--- src/web-awesome-based/main.js
+++ src/web-awesome-based/main.js
@@ -1,12 +1,25 @@
-import "https://early.webawesome.com/webawesome@3.0.0-alpha.9/dist/components/button/button.js"
-import "https://early.webawesome.com/webawesome@3.0.0-alpha.9/dist/components/dropdown/dropdown.js"
-import "https://early.webawesome.com/webawesome@3.0.0-alpha.9/dist/components/menu/menu.js"
-import "https://early.webawesome.com/webawesome@3.0.0-alpha.9/dist/components/menu-item/menu-item.js"
-import "https://early.webawesome.com/webawesome@3.0.0-alpha.9/dist/components/icon/icon.js"
-import "https://early.webawesome.com/webawesome@3.0.0-alpha.9/dist/components/input/input.js"
-// import 'https://early.webawesome.com/webawesome@3.0.0-alpha.7/dist/components/dialog/dialog.js'
+import "https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/button/button.js"
+import "https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/dropdown/dropdown.js"
+import "https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/menu/menu.js"
+import "https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/menu-item/menu-item.js"
+import "https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/icon/icon.js"
+import "https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/input/input.js"
+import 'https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/checkbox/checkbox.js';
+import 'https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/icon-button/icon-button.js';
+import 'https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/textarea/textarea.js';
+import 'https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/card/card.js';
+import 'https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/dialog/dialog.js';
+import 'https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/tag/tag.js';
+import 'https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/switch/switch.js';
+import 'https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/components/avatar/avatar.js';
 
 import "./button.js"
 import "./dropdown.js"
 import "./input.js"
-// import "./dialog.js"
+import "./checkbox.js"
+import "./icon-button.js"
+import "./textarea.js"
+import "./dialog.js"
+import "./tag.js"
+import "./toggle.js"
+import "./avatar.js"
