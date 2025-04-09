fc29bb2a4225de92deb616d1c5bfeb683a988ee3
ail3n <ailenrgrimaldi@gmail.com>
2025-04-09 12:12:53 -0300

    slowly change way to add stylesheets

--- src/web-awesome-based/utils.js
+++ src/web-awesome-based/utils.js
@@ -2,3 +2,7 @@
 export const tagFn = fn => (strings, ...parts) => fn(parts.reduce((tpl, value, i) => `${tpl}${strings[i]}${value}`, '').concat(strings[parts.length]))
 export const html = tagFn(s => new DOMParser().parseFromString(`<template>${s}</template>`, 'text/html').querySelector('template'))
 export const css = tagFn(s => s)
+
+export const tagFn2 = fn => (strings, ...parts) => fn(parts.reduce((tpl, value, i) => `${tpl}${strings[i]}${value}`, '').concat(strings[parts.length]))
+export const html2 = tagFn2(s => new DOMParser().parseFromString(`<template>${s}</template>`, 'text/html').querySelector('template'))
+export const css2 = tagFn2(s => new CSSStyleSheet().replace(s))
